c2a84df7cfbf63f926eb7dac5a295b2290d49334
denverparie <denverparie@gmail>
2020-10-15 10:20:38 +0200

    second step

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -33,11 +33,10 @@ public class InfoActivity extends AppCompatActivity {
         Uri u = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,u);
         mapIntent.setPackage("com.google.android.apps.maps");
-
-
-        if(mapIntent.resolveActivity(getPackageManager())==null){
+        if(mapIntent.resolveActivity(getPackageManager())!=null){
             startActivity(mapIntent);
         }
+
     }
 
     // ***
