fc688a47109fe306e310f9c837e6ddaaac469f65
denverparie <denverparie@gmail>
2020-10-15 08:51:32 +0200

    firstIntentDone

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
     // ***
-    // TODO - Task 1 - Show Store Information Activity
+
     // ***
 
     @Override
